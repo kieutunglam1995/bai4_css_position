e74ad2f2823c60b41e9598ce09786702767efcb9
LamTung <kieutunglam1995@gmail.com>
2023-03-21 19:28:46 +0700

    first commit

---
+++ bai4.js
@@ -0,0 +1,9 @@
+const myImage = document.getElementById("myImage");
+const message = document.getElementById("message");
+    console.log(message)
+setInterval(function(){
+    message.textContent="chuối thơm quá";
+},3000)
+;
+
+
